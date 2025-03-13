59666f0f0711e0793ce36d3267eb9f185f35ace1
NikitaAndreychuk <andreychuk.nikita0@gmail.com>
2025-03-13 19:08:30 +0300

    Added new tasks to Ansible

---
+++ docker/ansible/task_3/create_ssh_config.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /home/iziperr/Documents/Modsen-Practice/courses/ansible/task_1
+vagrant ssh-config ubuntu > ../task_3/vagrant_ubuntu_ssh
+vagrant ssh-config fedora > ../task_3/vagrant_fedora_ssh
